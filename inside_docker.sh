5c61c2c17f55f5dddf99605cfb962f5c68f15839
Marisa Kirisame <marisa@cs.utah.edu>
2023-09-30 19:58:48 -0600

    save

--- inside_docker.sh
+++ inside_docker.sh
@@ -3,7 +3,7 @@ set -e
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y git ssh-client build-essential pkg-config
+apt-get install -y git ssh-client build-essential pkg-config libssl-dev
 
 cat ~/.ssh/id_rsa.pub || ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
 
