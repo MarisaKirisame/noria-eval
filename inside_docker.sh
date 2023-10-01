2d6eef4b327d7186f0a6c0c389169e1a9e0966b6
Marisa Kirisame <marisa@cs.utah.edu>
2023-09-30 18:02:15 -0600

    save

--- inside_docker.sh
+++ inside_docker.sh
@@ -1,5 +1,9 @@
+set -x
+
 apt-get update
+apt-get upgrade -y
 apt-get install -y git
+
 ls ~/.ssh/id_rsa.pub || ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
 rm -rf noria || true # rust does not track C++ ffi correctly and git submodule suck. just recompile everytime.
 git clone git@github.com:MarisaKirisame/noria.git
