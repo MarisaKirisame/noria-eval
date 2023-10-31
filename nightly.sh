6e92db60ea690948b65fdd729a98c7d145ee8d19
Marisa Kirisame <marisa@cs.utah.edu>
2023-10-31 01:05:25 -0600

    save

--- nightly.sh
+++ nightly.sh
@@ -2,7 +2,8 @@ set -x
 set -e
 
 docker ps
-(docker run -d --name noria-eval zookeeper) || true
+docker start noria-eval || true
+docker run -d --name noria-eval zookeeper || true
 docker exec -i noria-eval bash < inside_docker.sh
 
 docker cp noria-eval:output ./
